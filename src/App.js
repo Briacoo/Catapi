fd3247a811685e8fc762c5b7389cb412b5cdc661
Onno <onnobriac@gmail.com>
2023-03-24 14:29:34 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Rect
+          Learn Reaaaaaaact
+          rbfgynuh
         </a>
       </header>
     </div>
